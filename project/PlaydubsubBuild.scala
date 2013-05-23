7298cdcea79726681ef88abad53be86348bd5f53
Alex Jarvis <alex@panaxiom.co.uk>
2013-05-23 12:36:00 +0100

    Prepare next version

--- project/PlaydubsubBuild.scala
+++ project/PlaydubsubBuild.scala
@@ -9,12 +9,12 @@ object PlaydubsubBuild extends Build {
     settings = Project.defaultSettings ++ Seq(
       name := "play-dubsub",
       organization := "uk.co.panaxiom",
-      version := "0.1-SNAPSHOT",
+      version := "0.2-SNAPSHOT",
       scalaVersion := "2.10.1",
       // add other settings here
       libraryDependencies ++= Seq(
-        "play" %% "play" % "2.1.1",
-        "uk.co.panaxiom" % "dubsub" % "0.1-SNAPSHOT"
+        "play" %% "play" % "2.1.1" % "provided",
+        "uk.co.panaxiom" % "dubsub" % "0.2-SNAPSHOT"
       ),
       resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
     )
